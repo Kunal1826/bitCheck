629c8dbb0f4324db24e188f56b6b2d0a996a5d5a
kunal silawat <kunal.kumawat001@gmail.com>
2025-05-16 04:28:26 +0530

    another one

--- backend/src/app.js
+++ backend/src/app.js
@@ -7,11 +7,23 @@ const AIRoutes = require("./routes/AIRoutes/ai.route.js")
 const userRoutes = require("./routes/userRoutes/user.route.js")
 const uploadCodeRoute = require("./routes/codesRutes/codeRoute.js")
 
+
+const allowedOrigins = [
+  "http://localhost:5173", 
+  "https://bitcheck-frontend.onrender.com"
+];
+
 app.use(cors({
-    origin: "http://localhost:5173",
-    credentials: true,
-}
-))
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  credentials: true, 
+}));
+
 app.use(cookieparser())
 
 
