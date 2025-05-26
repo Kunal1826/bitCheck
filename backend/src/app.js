2089a0e727dd8aed1583f93544873fed59ca5afb
kunal silawat <kunal.kumawat001@gmail.com>
2025-05-26 12:12:38 +0530

    another one

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,13 +1,12 @@
 const express = require('express');
-const path = require('path');
+app = express();
 const cookieparser = require("cookie-parser");
-const cors = require("cors");
+const cors = require("cors")
 
-const AIRoutes = require("./routes/AIRoutes/ai.route.js");
-const userRoutes = require("./routes/userRoutes/user.route.js");
-const uploadCodeRoute = require("./routes/codesRutes/codeRoute.js");
+const AIRoutes = require("./routes/AIRoutes/ai.route.js")
+const userRoutes = require("./routes/userRoutes/user.route.js")
+const uploadCodeRoute = require("./routes/codesRutes/codeRoute.js")
 
-const app = express();
 
 const allowedOrigins = [
   "http://localhost:5173", 
@@ -25,19 +24,14 @@ app.use(cors({
   credentials: true, 
 }));
 
-app.use(cookieparser());
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
-app.use("/api/ai", AIRoutes);
-app.use("/api/user", userRoutes);
-app.use("/api/code", uploadCodeRoute);
-
-app.use(express.static(path.join(__dirname, "../../frontend/build")));
+app.use(cookieparser())
 
 
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../../frontend/build", "index.html"));
-});
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+
+app.use("/api/ai",AIRoutes)
+app.use("/api/user",userRoutes)
+app.use("/api/code",uploadCodeRoute)
 
 module.exports = app;
