4000cbfca1dff8bfc0306a3cce6399f0ee2af9de
kunal silawat <kunal.kumawat001@gmail.com>
2025-05-26 11:55:38 +0530

    another one

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,12 +1,13 @@
 const express = require('express');
-app = express();
+const path = require('path');
 const cookieparser = require("cookie-parser");
-const cors = require("cors")
+const cors = require("cors");
 
-const AIRoutes = require("./routes/AIRoutes/ai.route.js")
-const userRoutes = require("./routes/userRoutes/user.route.js")
-const uploadCodeRoute = require("./routes/codesRutes/codeRoute.js")
+const AIRoutes = require("./routes/AIRoutes/ai.route.js");
+const userRoutes = require("./routes/userRoutes/user.route.js");
+const uploadCodeRoute = require("./routes/codesRutes/codeRoute.js");
 
+const app = express();
 
 const allowedOrigins = [
   "http://localhost:5173", 
@@ -24,14 +25,19 @@ app.use(cors({
   credentials: true, 
 }));
 
-app.use(cookieparser())
+app.use(cookieparser());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use("/api/ai", AIRoutes);
+app.use("/api/user", userRoutes);
+app.use("/api/code", uploadCodeRoute);
+
+app.use(express.static(path.join(__dirname, "../../frontend/build")));
 
 
-app.use(express.json())
-app.use(express.urlencoded({extended:true}))
-
-app.use("/api/ai",AIRoutes)
-app.use("/api/user",userRoutes)
-app.use("/api/code",uploadCodeRoute)
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../../frontend/build", "index.html"));
+});
 
 module.exports = app;
